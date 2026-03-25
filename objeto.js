f6a393ba9c14da09d59d27a0bb08dab7b7001495
isabellysilvaxavier-max <isabelly.silva.xavier@escola.pr.gov.br>
2026-03-25 11:06:44 -0300

    Add files via upload

---
+++ objeto.js
@@ -0,0 +1,8 @@
+let pessoa = {
+    Nome: "João"
+    idade: 30;
+    profissao: string;
+}
+console.log(pessoa.idade);
+
+    
---
+++ objeto2.js
@@ -0,0 +1,22 @@
+let carro ={
+marca: "Nissan",
+modelo:"kicks",
+ano:30,
+
+ligar: function(){
+    console.log("o carro ligou");
+},
+  mostrarInfo: function(){
+    console.log('marca $ {this.marca},\nModelo:${this.modelo}')
+
+
+
+
+
+
+  }
+
+
+
+
+}
